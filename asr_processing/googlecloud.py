f958f34e87af2fe1a0939cf423ad09bfdc5ffe2d
xavieryn <61944157+xavieryn@users.noreply.github.com>
2024-09-29 17:38:08 -0400

    Has not been tested because I don't really know how to run the data :(

--- asr_processing/googlecloud.py
+++ asr_processing/googlecloud.py
@@ -2,21 +2,43 @@ from utils.drive_utils import read_folder_and_modify
 import pandas as pd
 import os
 ### STEP1: IMPORT LIBRARIES
+from google.cloud import speech
+import io
+
+# Instantiates a client from google cloud
+client = speech.SpeechClient()
 
 ### STEP2: DEFINE MODEL
+# What do i put here??
 
 transcriptions = []
 
 def transcribe_wav_file(data, file_name):
-    with open('temp.wav', 'wb') as f:
-        f.write(data.read())
+    answer = ''
+    # Prepare the RecognitionAudio object
+    with io.open(temp.wav, "wb") as audio_file:
+        content = audio_file.read()
+    audio = speech.RecognitionAudio(content=content)
 
-    result = ### STEP3: SAVE TRANSCRIPTION OUTPUT (STRING TYPE) | file_path = 'temp.wav'
+        # Set up the RecognitionConfig
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,  # Ensure this matches your file format
+        sample_rate_hertz=8000, 
+        language_code="en-US",  # Language of the audio
+    )
+
+    # Call the API to transcribe the audio
+    response = client.recognize(config=config, audio=audio)
+
+    # Process the response and save the transcript to answer
+    for resultString in response.results:
+        answer += resultString.alternatives[0].transcript
 
     transcriptions.append({
         'file_name': file_name,
-        'prediction': result.lower()
+        'prediction': answer.lower()
     })
+    
     if os.path.exists('temp.wav'):
         os.remove('temp.wav')
     return None
