296673129e0380977a1503a707f1c45952436db2
Xavier Nishikawa <xavier.nishikawa@gmail.com>
2024-11-14 23:55:22 -0500

    Starting to use Dongims model to tokenize stutter data

---
+++ FineTune/whisperTuned.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import os
+import librosa
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+
+# model = load_model("base")
+predicted_transcriptions = []
+
+# Load model directly
+
+processor = AutoProcessor.from_pretrained("dongim04/whisper-base-en")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("dongim04/whisper-base-en")
+
+def transcribe_wav_file(data, file_name):
+    with open('temp.wav', 'wb') as f:
+        f.write(data.read())
+    input_audio, _ = librosa.load('temp.wav', sr=16000)
+    out = model.transcribe(input_audio)      
+    result = out['text']
+    
+    predicted_transcriptions.append({
+        'file_name': file_name,
+        'prediction': result.lower()
+    })
+    if os.path.exists('temp.wav'):
+        os.remove('temp.wav')
+    return None
+
+def save_transcriptions_to_csv(audio_csv_file_name):
+    df = pd.DataFrame(predicted_transcriptions)
+    df.to_csv(audio_csv_file_name)
+    
+input_folder_id = '1AdEu1i1kkuQIRBCEC6MqlpGSk0EGHUy-' # LibriSpeech train-clean-100
+# # output_file_path = '/home/ec2-user/stuttered-speech-asr/librispeech_result/whisper_librispeech.csv'
+output_file_path = 'C:/Users/dlee3/OneDrive - Olin College of Engineering/PInT/stuttered-speech-asr/librispeech_result/whisper_librispeech.csv'
+
+
+# read_folder_and_process(input_folder_id, transcribe_wav_file, '')
+# save_transcriptions_to_csv(output_file_path)
